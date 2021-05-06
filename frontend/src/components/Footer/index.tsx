bf8effdb09b8373bb88a172ce86c99c0836f5cfa
fellipySaldanha <fellipy.saldanha@gmail.com>
2021-05-06 08:57:46 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="#" target="_blank" rel="noreferrer">Fellipy Saldanha</a></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
