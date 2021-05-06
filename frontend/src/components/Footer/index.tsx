d2ae535b382bf75cc7e946cf9acfe4739db7fb04
fellipySaldanha <fellipy.saldanha@gmail.com>
2021-05-06 10:19:06 -0300

    link

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -2,7 +2,7 @@ function Footer() {
     return (
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
-                <p className="text-light">App desenvolvido por <a href="#" target="_blank" rel="noreferrer">Fellipy Saldanha</a></p>
+                <p className="text-light">App desenvolvido por <a href="https://github.com/fellipySaldanha/projeto-sds3" target="_blank" rel="noreferrer">Fellipy Saldanha</a></p>
             </div>
         </footer>
     );
