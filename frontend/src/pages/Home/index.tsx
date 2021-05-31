9b24f37d4e78e04207036236ca0243e2b2ca9d93
fellipySaldanha <fellipy.saldanha@gmail.com>
2021-05-31 17:26:46 -0300

    routes

---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import ImgDsDark from 'assets/img/ds-dark.svg';
+function Home() {
+    return (
+        <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
+           home
+        </div>
+    );
+}
+
+export default Home;
