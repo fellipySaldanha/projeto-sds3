138a39c09dee0a111b0616395a7b7fef2ae06652
fellipySaldanha <fellipy.saldanha@gmail.com>
2021-06-09 08:26:14 -0300

    dataTable integration

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -28,7 +28,7 @@ function BarChart() {
         }]
     });
     useEffect(() => {
-        axios.get(`${BASE_URL}/sales/sucess-by-seller`)
+        axios.get(`${BASE_URL}/sales/success-by-seller`)
             .then((response) => {
                 const data = response.data as SaleSucess[];
                 const myLabels = data.map(x => x.sellerName);
